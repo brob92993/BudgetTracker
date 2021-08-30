70acd95c9b464e7d4ceb5f248f9613c953b344dc
Brian Robinson <brob92993@gmail.com>
2021-08-30 15:11:21 -0400

    added seeders folder with seed.js file for the database

---
+++ seeders/seed.js
@@ -0,0 +1,36 @@
+var mongoose = require("mongoose");
+var db = require("../models");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/imageperformance",
+  { useNewUrlParser: true }
+);
+
+var transactionSeed = [
+    {
+      name: "Groceries",
+      value: 200,
+      date: new Date(Date.now())
+    },
+    {
+        name: "Television",
+        value: 900,
+        date: new Date(Date.now())
+    },
+    {
+        name: "Gym Clothes",
+        value: 100,
+        date: new Date(Date.now())
+    }
+];
+
+db.Transaction.deleteMany({})
+  .then(() => db.Transaction.collection.insertMany(transactionSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
